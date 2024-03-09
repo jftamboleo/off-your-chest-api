4b193a56423d0d0c570928ab4316b6c3ae28e617
Jose <jfernandeztamboleo@gmail.com>
2024-03-09 17:27:46 +0100

    add uploadRandomPost and cors

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const cors = require('cors')
 const { getRandomPost } = require('./utils')
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.get('/api/v1/posts/', async (req, res) => {
   const randomPost = await getRandomPost()
   res.json(randomPost)
+})
+
+app.post('/api/v1/posts/', (req, res) => {
+  const { title, body } = req.body
+  const newPost = {
+    id: crypto.randomUUID(),
+    title,
+    body,
+    date: new Date()
+  }
+  // TODO: add newPost to db
+  res.json(newPost)
 })
 
 const PORT = process.env.PORT
